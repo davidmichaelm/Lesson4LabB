9922c45e6a5e2e4828bfc72817ca0f1098e6184b
David Marquardt <davemarquardt@gmail.com>
2021-04-15 20:38:34 -0500

    Initial commit

---
+++ Lesson4LabB/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lesson4LabB
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Entry_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            Label.Text = e.NewTextValue;
+        }
+
+        private void Uppercase_Button_OnClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Editor.Text))
+                Editor.Text = Editor.Text.ToUpper();
+
+            if (!string.IsNullOrEmpty(Entry.Text))
+            {
+                Label.Text = Entry.Text.ToUpper();
+                Entry.Text = Entry.Text.ToUpper();
+            }
+        }
+        
+        private void Lowercase_Button_OnClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Editor.Text))
+                Editor.Text = Editor.Text.ToLower();
+
+            if (!string.IsNullOrEmpty(Entry.Text))
+            {
+                Label.Text = Entry.Text.ToLower();
+                Entry.Text = Entry.Text.ToLower();
+            }
+        }
+    }
+}
